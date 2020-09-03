c0fc5bca8c2b8f2e128f60407b2b6ac405644aec
Bianca Benedictos <bianca_benedictos@dlsu.edu.ph>
2020-09-03 23:24:15 +0800

    created routers

--- router/viewAnalyticsRoutes.js
+++ router/viewAnalyticsRoutes.js
@@ -2,9 +2,8 @@ const express = require('express');
 const router = express();
 
 /* controllers that provide functions depending on post or get */
-const controller = require('../controllers/indexController');
+const controller = require('../controllers/viewAnalyticsController');
 
 /* routes */
-router.get("/" , controller.getDashboard);
 
 module.exports = router;
