e02723ad5738e345216c70f5ef5e75dd2111079b
Bianca Benedictos <bianca_benedictos@dlsu.edu.ph>
2020-09-03 21:47:01 +0800

    converted to HBS, added nav bar builder

--- index.js
+++ index.js
@@ -8,9 +8,38 @@ const indexRoutes = require('./router/indexRoutes');
 app.use('/', indexRoutes);
 
 /** Helper Functions **/
+hbs.registerHelper("navBuilder", (sPage, sUserType)=>{
+    let element = '';
+    let navs = ['Dashboard', 'Profile', 'Records', 'Send Request', 'Pending Requests', 'View Analytics', 'Manage Accounts', 'Send Notification','Holidays', 'Logout'];
+    let url = ['/dashboard', '/profile', '/records', '/send-request', '/pending-requests', '/view-analytics', '/manage-accounts', '/send-notification','/holidays', '/logout'];
+    let visible = [];
 
+    if (sUserType === 'Student Representative'){
+        visible = [true, true, true, true, false, false, false, false, false, true];
+    }
+    else if (sUserType === 'Human Resource Representative'){
+        visible = [true, true, true, true, true, true, false, true, false, true];
+    }
+    else{
+        visible = [true, true, true, false, true, true, true, true, true, true];
+    }
+
+    let i=0;
+    while (i<navs.length){
+        if (visible[i]){
+            if (sPage === navs[i]){
+                element += '<a class="nav-link active" href="'+ url[i] + '">' + navs[i] + '</a>';
+            }
+            else{
+                element += '<a class="nav-link" href="'+ url[i] + '">' + navs[i] + '</a>';
+            }
+        }
+        i++;
+    }
+    return new hbs.SafeString(element);
+});
 
 /** Server online **/
 app.listen(process.env.PORT, process.env.LOCAL_ADDRESS, ()=>{
     console.log("Server ready.");
-})
+});
