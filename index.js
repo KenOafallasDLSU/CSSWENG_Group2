11357151ede21a91f56b6b33326c834ffb708288
KenOafallasDLSU <kenneth_oafallas@dlsu.edu.ph>
2020-09-09 20:58:44 +0800

    Fixed and readded database code

--- index.js
+++ index.js
@@ -1,14 +1,66 @@
+/**Dependencies */
 const express = require("express");
 const hbs = require("hbs");
+const bodyParser = require('body-parser');
+const mongodb = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 
+/**Engine creation */
 const index = express();
 
 const port = 3000;
+
+/**Database constants */
+const mongoClient = mongodb.MongoClient;
+const databaseURL = "mongodb://localhost:27017/residencydb"; //local URL
+//const databaseURL = "mongodb+srv://Admin:a1b2c3d4%21@occs-residency-system.jflhq.gcp.mongodb.net/residencydb?retryWrites=true&w=majority"; //Atlas URL
+const dbname = "residencydb";
+
+/**Database models */
+const modelCUH = require('./models/DB_CUH');
+const modelSRep = require('./models/DB_SRep');
+const modelSuspension = require('./models/DB_Suspension');
+const modelTimeLog = require('./models/DB_TimeLog');
+const modelTimeRequest = require('./models/DB_TimeRequest');
+
+/**Create DB Collections if they do not exist */
+const options = { useUnifiedTopology: true };
+mongoClient.connect(databaseURL, options, function(err, client) {
+  console.log("Connected to DB at " + databaseURL);
+  if (err) throw err;
+  const dbo = client.db(dbname);
+
+  dbo.createCollection("colCUHs", function(err, res) {
+    //if (err) throw err;
+
+    dbo.createCollection("colSReps", function(err, res) {
+      //if (err) throw err;
+
+      dbo.createCollection("colSuspensions", function(err, res) {
+        //if (err) throw err;
+
+        dbo.createCollection("colTimeLogs", function(err, res) {
+          //if (err) throw err;
+
+          dbo.createCollection("colTimeRequests", function(err, res) {
+            //if (err) throw err;
+            console.log("All collections created!");
+            client.close();
+          });
+        });
+      });
+    });
+  });
+});
 
 /**** Partials ****/
 hbs.registerPartials(__dirname+ "/views/partials");
 
 index.set("view engine", "hbs");
+
+/**Configuration for handling API endpoint data */
+index.use(bodyParser.json());
+index.use(bodyParser.urlencoded({ extended: true }));
 
 /********* Routing *********/
 const indexRoutes = require('./router/indexRoutes');
@@ -67,6 +119,9 @@ hbs.registerHelper("navBuilder", (sPage, sUserType)=>{
     }
     return new hbs.SafeString(element);
 });
+
+/**Static hosting */
+index.use(express.static('public'));
 
 /** Server online **/
 index.listen(port, function()
