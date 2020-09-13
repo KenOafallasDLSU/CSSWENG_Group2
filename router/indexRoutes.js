41399bf2d2674a1004212098210104a03b66b094
Bianca Benedictos <bianca_benedictos@dlsu.edu.ph>
2020-09-13 17:13:36 +0800

    added authentication and validation

--- router/indexRoutes.js
+++ router/indexRoutes.js
@@ -4,17 +4,20 @@ const router = express();
 /* controllers that provide functions depending on post or get */
 const controller = require('../controllers/indexController');
 const validation = require('../middlewares/validation');
+const authentication = require('../middlewares/authentication.js');
 
-/* routes */
-router.get("/" , controller.getDashboard);
+/********* routes *********/
 
 /* login */
-router.get("/login", /* insert session authentication,*/ controller.getLogin);
-router.post("/login", /* insert loginValidation(),*/ controller.postLogin);
+router.get('/login', authentication.sessionNotActive, controller.getLogin);
+router.post('/login', validation.loginValidation(), controller.postLogin);
 
 /* register */
 router.get('/register', controller.getRegister);
-router.post('/register', validation.registerValidation(),controller.postRegister);
+router.post('/register', validation.registerValidation(), controller.postRegister);
 router.get('/checkID', controller.checkID);
+
+/* logout */
+router.get('/logout', controller.getLogout);
 
 module.exports = router;
