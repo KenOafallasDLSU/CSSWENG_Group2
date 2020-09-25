768130ca2b6d3096ad40b901a8e23a6b641ae7b2
Bianca Benedictos <bianca_benedictos@dlsu.edu.ph>
2020-09-25 18:44:13 +0800

    Revert "added middlewares"
    This reverts commit 92fe7ebce089bb6600323c24b300f857a5dc001a.

--- router/sRepRoutes.js
+++ router/sRepRoutes.js
@@ -3,11 +3,10 @@ const router = express();
 
 /* controllers that provide functions depending on post or get */
 const controller = require('../controllers/sRepController');
-const authentication = require('../middlewares/sRepAuthentication.js');
 
 /********* routes *********/
 
 /* dashboard */
-router.get(['/:sUsername', '/dashboard/:sUsername'], authentication.sessionActive, authentication.isValidSRep, controller.getDashboard);
+router.get(['/', '/dashboard'] , controller.getDashboard);
 
 module.exports = router;
