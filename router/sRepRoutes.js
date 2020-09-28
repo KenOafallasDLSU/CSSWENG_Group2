fcd42a419b22089d5f927c2a50cdd03d81a612fb
Bianca Benedictos <bianca_benedictos@dlsu.edu.ph>
2020-09-28 21:38:42 +0800

    slight modification for send req

--- router/sRepRoutes.js
+++ router/sRepRoutes.js
@@ -26,11 +26,8 @@ router.get(['/:sUsername', '/dashboard2/:sUsername'] , authentication.sessionAct
 
 router.post(['/:sUsername', '/dashboard2/:sUsername'] , authentication.sessionActive, authentication.isValidSRep, timeLogControllers.postTimeOut);
 
-
-
-router.get(['/:sUsername', '/sendRequest/:sUsername'] , authentication.sessionActive, authentication.isValidSRep, controller.getSendRequest);
-// router.get("/" , timeLogControllers.getSendRequest);
-
-router.post(['/:sUsername', '/sendRequest/:sUsername'] , authentication.sessionActive, authentication.isValidSRep, timeLogControllers.postSendRequest);
+// send request paths
+router.get(['/send-request', '/send-request/:sUsername'] , authentication.sessionActive, authentication.isValidSRep, controller.getSendRequest);
+router.post(['/send-request', '/sendRequest/:sUsername'] , authentication.sessionActive, authentication.isValidSRep, timeLogControllers.postSendRequest);
 
 module.exports = router;
