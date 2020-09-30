f5e36924bf24a386d803dad3f02b402ab12cfe47
KenOafallasDLSU <kenneth_oafallas@dlsu.edu.ph>
2020-09-30 23:38:09 +0800

    HR routes

---
+++ middlewares/hrAuthentication.js
@@ -0,0 +1,69 @@
+const { check } = require('express-validator');
+
+/* database */
+const db = require('../models/db.js');
+const modelSREP = require('../models/DB_SRep.js');
+const modelCUH = require('../models/DB_CUH.js');
+
+const hrAuthentication = {
+    
+    /***** Check if Session is Active *****/
+    sessionActive: (req, res, next)=>{
+        if (req.session.userId){
+            console.log("active session, next lang");
+            next();
+        }
+        else{
+            console.log("no active session, redirect");
+            return res.redirect('/login');
+        }
+    },
+    
+    isValidHR: (req, res, next)=>{        
+        if(req.params.sUsername == req.session.userId){
+            db.findOne(modelSREP, {sUsername: req.session.userId, bHRStatus: true}, '', function(objSREP){
+                if (objSREP){
+                    next();
+                }
+                else{
+                    db.findOne(modelCUH, {sUsername: req.session.userId}, '', function(objCUH){
+                        if (objCUH){
+                            return res.redirect('/cuh/' + objCUH.sUsername);
+                        }
+                        else{
+                            return res.redirect('/logout');
+                        }
+                    });
+                }
+            });
+        }else{
+            db.findOne(modelSREP, {sUsername: req.session.userId, bHRStatus: true}, '', function(objSREP){
+                if (objSREP){
+                    return res.redirect('/srep/' + objSREP.sUsername);
+                }
+                else{
+                    db.findOne(modelCUH, {sUsername: req.session.userId}, '', function(objCUH){
+                        if (objCUH){
+                            return res.redirect('/cuh/' + objCUH.sUsername);
+                        }
+                        else{
+                            return res.redirect('/logout');
+                        }
+                    });
+                }
+            });
+        }
+    },
+
+    redirect: (req, res, next) =>{
+        if(req.params.sUsername === req.session.userId){
+            next();
+        }
+        else{
+            return res.redirect(req.url + '/' + req.session.userId);
+        }
+    },
+	
+}
+
+module.exports = hrAuthentication;
