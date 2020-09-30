83e3b988dd71c593e1a1ffdfd4ff442ddece8faf
Poppipa <47263301+Popippa@users.noreply.github.com>
2020-09-30 10:27:13 +0800

    ManangeAccounts (updated)

--- public/js/manageAccounts.js
+++ public/js/manageAccounts.js
@@ -1,33 +1,32 @@
 $(document).ready(function() {
   
     $('#btnSubmitCreateCUH').click(function() {
+		
         var sFirstName = $('#sFirstName').val();
         var sLastName = $('#sLastName').val();
         var sEmail = $('#sEmail').val();
         var sUserName = $('#sUsername').val();
         var sPassword = $('#sPassword').val();
+		console.log("clicked me");
 
-        $.post('/manage-accounts/postCreateCUH', { sFirstName:sFirstName , sLastName:sLastName , sEmail:sEmail , sUserName:sUserName , sPassword:sPasswrord  }, function(){
+        $.post('/manage-accounts/postCUHRegister', { sFirstName:sFirstName , sLastName:sLastName , sEmail:sEmail , sUserName:sUserName , sPassword:sPasswrord  }, function(){
             
         });
 
     });
 
-
-
-    $('#btnSubmitGrant').click(function() {
-        
+    $('#btnSubmitGrant').click(function() {        
         var sUserName = $('#sUsername').val();
-        $.post('/manage-accounts/postGrantHRAccess', {sUserName:sUserName}, function(){
+        $.post('/manage-accounts/postAccept', {sUserName:sUserName}, function(){
            
         });
  
     });
 
-    $('#btnSubmiRevoke').click(function() {
+    $('#btnSubmitRevoke').click(function() {
         var sUserName = $('#sUsername').val();
-        $.post('/manage-accounts/postRevokeHRAccess', {sUserName:sUserName}, function(){
-           
+        $.post('/manage-accounts/postRevoke', {sUserName:sUserName}, function(){
+         console.log("clicked me");  
         });
   
     });
